62777e850926a5421a887d160934eacfc250ee02
Lcs-93 <baillylucas04@gmail.com>
2025-04-03 09:52:26 +0200

    modif securité

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import CategorieRoute from './routes/CategorieRoute.js';
 import ProduitRoute from './routes/ProduitRoute.js';
 import StatsRoute from './routes/StatsRoute.js';
 import { Categorie, Produit } from './models/index.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(express.json());
 
@@ -13,8 +14,8 @@ app.use('/produits', ProduitRoute);
 app.use('/stats', StatsRoute);
 
 sequelize.sync({ alter: true }).then(() => {
-    app.listen(4000, () => {
-        console.log("✅ Serveur démarré sur le port 4000");
+    app.listen(process.env.PORT, () => {
+        console.log(`✅ Serveur démarré sur le port ${process.env.PORT}`);
     });
 }).catch(error => {
     console.error("❌ Erreur de connexion à la base de données : ", error);
